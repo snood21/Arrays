7306e06600c045993215134b3e61f5b5ef7adbe1
snood <snood21@gmail.com>
2022-11-05 18:49:47 +0300

    deleted old files

--- ArraysMain/NonTemplatedFunctions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "TemplatedFunctions.h"
-
-void FillRand(float*& Array, const int ArraySize, bool& ArrayFilled, int& ActiveString, int MinRand = 0, int MaxRand = 255);
-void FillRand(float**& Array, const int ArrayColumns, const int ArrayStrings, bool& ArrayFilled, int& ActiveString, int MinRand = 0, int MaxRand = 255);
-
-void FillRand(double*& Array, const int ArraySize, bool& ArrayFilled, int& ActiveString, int MinRand = 0, int MaxRand = 255);
-void FillRand(double**& Array, const int ArrayColumns, const int ArrayStrings, bool& ArrayFilled, int& ActiveString, int MinRand = 0, int MaxRand = 255);
